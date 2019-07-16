ee999f0c668eb61dd74233ae33c2f378456c938b
James Jacoby <james@mobyinc.com>
2019-07-16 14:25:01 -0700

    adds initial state

--- src/game-room.ts
+++ src/game-room.ts
@@ -1,20 +1,52 @@
 import { Room } from "colyseus";
-import { State } from './state'
+import { State, Ship } from './state'
+import { string } from "@colyseus/schema/lib/encoding/decode";
 
-export class GameRoom extends Room {
+const shipTypes = [
+    {
+        id: 1,
+        name: 'scout',
+        size: 2
+    },
+    {
+        id: 2,
+        name: 'cruiser',
+        size: 3
+    },
+    {
+        id: 3,
+        name: 'carrier',
+        size: 5
+    }
+];
+
+export class GameRoom extends Room<State> {
     maxClients = 2;
+    gridSize = 8;
+
+    placements: Array<Array<number>>;
+
+    players: Array<any>;
 
     onInit (options) {
         console.log("Room created!", options);
-        this.setState(new State());
+        this.reset();
+        this.players = new Array<any>();
+        // this.setState(new State());
     }
 
     onJoin (client) {
         this.broadcast(`${ client.sessionId } joined.`);
+        this.players.push({ client, seat: this.players.length });
+
+        // if (this.players.length == 2) {
+            this.state.phase = 'place';
+        // }
     }
 
     onLeave (client) {
         this.broadcast(`${ client.sessionId } left.`);
+        this.state.phase = 'waiting';
     }
 
     onMessage (client, data) {
@@ -23,7 +55,36 @@ export class GameRoom extends Room {
     }
 
     onDispose () {
-        console.log("Dispose BasicRoom");
+        console.log("Room destroyed!");
+    }
+
+    reset() {
+        this.placements = new Array<Array<number>>();
+        this.placements[0] = new Array<number>();
+        this.placements[1] = new Array<number>();
+
+        let cellCount = this.gridSize * this.gridSize;
+
+        let state = new State();
+
+        state.phase = 'waiting';
+        state.playerTurn = 0;
+        state.winningPlayer = -1;
+
+        for (var i=0; i<cellCount; i++) {
+            this.placements[0][i] = 0;
+            this.placements[1][i] = 0;
+            
+            state.player1Shots[i] = '?';
+            state.player2Shots[i] = '?';
+        }
+
+        for (let shipType of shipTypes) {
+            state.player1Ships[shipType.id] = Ship.create(shipType.name, shipType.size);
+            state.player2Ships[shipType.id] = Ship.create(shipType.name, shipType.size);
+        }
+
+        this.setState(state);
     }
 
 }
--- src/state.ts
+++ src/state.ts
@@ -1,6 +1,48 @@
-import { Schema, type, MapSchema } from "@colyseus/schema";
+import { Schema, type, MapSchema, ArraySchema } from "@colyseus/schema";
+
+export class Ship extends Schema {
+    @type('string')
+    name: string;
+
+    @type('number')
+    size: number;
+
+    @type('number')
+    health: number;
+
+    @type('string')
+    status: string;
+
+    static create(name: string, size: number): Ship {
+        var ship = new Ship();
+        ship.name = name;
+        ship.size = size;
+        ship.health = size;
+        ship.status = 'active';
+
+        return ship;
+    }
+}
 
 export class State extends Schema {
     @type('string')
-    hello: string;
+    phase: string = "waiting";
+
+    @type('number')
+    playerTurn: number = 1;
+
+    @type('number')
+    winningPlayer: number = 0;
+
+    @type({ map: Ship })
+    player1Ships: MapSchema<Ship> = new MapSchema<Ship>();
+
+    @type({ map: Ship })
+    player2Ships: MapSchema<Ship> = new MapSchema<Ship>();
+
+    @type(['string'])
+    player1Shots: ArraySchema<string> = new ArraySchema<string>();
+
+    @type(['string'])
+    player2Shots: ArraySchema<string> = new ArraySchema<string>();
 }
