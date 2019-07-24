2b68f62ae80ebd08f454315afffd95b3c7f26b3b
James Jacoby <james@mobyinc.com>
2019-07-24 15:55:23 -0700

    simplified state and game sequence logic

--- src/game-room.ts
+++ src/game-room.ts
@@ -1,6 +1,6 @@
 import { Room } from "colyseus";
-import { State, Ship } from './state'
-import { string } from "@colyseus/schema/lib/encoding/decode";
+import { State } from './state'
+import { string, number } from "@colyseus/schema/lib/encoding/decode";
 
 const shipTypes = [
     {
@@ -22,11 +22,11 @@ const shipTypes = [
 
 export class GameRoom extends Room<State> {
     maxClients = 2;
-    gridSize = 8;
-
+    gridSize: number = 8;
+    totalShipHealth: number = shipTypes.map((shipType) => shipType.size).reduce((a, b) => a + b);
+    playerHealth: Array<number>;
     placements: Array<Array<number>>;
-    playersPlaced: number;
-
+    playersPlaced: number = 0;
     players: Array<any>;
 
     onInit (options) {
@@ -37,12 +37,16 @@ export class GameRoom extends Room<State> {
     }
 
     onJoin (client) {
-        this.broadcast(`${ client.sessionId } joined.`);
-        this.players.push({ client, seat: this.players.length });
+        console.log(`${ client.sessionId } joined.`);
+        this.players.push({ sessionId: client.sessionId, seat: this.players.length + 1 });
 
-        // if (this.players.length == 2) {
+        if (this.players.length == 1) {
+            this.state.player1 = client.sessionId;
             this.state.phase = 'place';
-        // }
+        } else if (this.players.length == 2) {
+            this.state.player2 = client.sessionId;
+            // this.state.phase = 'place';
+        }
     }
 
     onLeave (client) {
@@ -53,38 +57,49 @@ export class GameRoom extends Room<State> {
     onMessage (client, message) {
         console.log("Received message from", client.sessionId, ":", message);
         
-        if (!message || !Array.isArray(message) || message.length === 0) return;
+        if (!message) return;
 
-        let player = this.players.find((player) => {
-            player.client.id === client.id
-        });
-
-        let command: string = message[0];
+        let player = this.players.find((player) => player.sessionId === client.sessionId);
 
         if (!player) return;
+
+        let command: string = message['command'];
 
         switch (command) {
             case 'place':
                 console.log('player ' + player.seat + ' placed ships');
+                this.placements[player.seat - 1] = message['placement'];
                 this.playersPlaced++;
 
-                if (this.playersPlaced == 2) {
+                if (this.playersPlaced == 1) {
+                    console.log('entering battle phase');
                     this.state.phase = 'battle';
                 }
                 break;
             case 'turn':
-                if (this.state.playerTurn != player.seat) return; // ignore if not your turn
+                if (this.state.playerTurn != player.seat) return; // ignore if not correct player
 
-                let target = message[1];
-                console.log('player ' + player.seat + ' targets ' + target);
+                let targetIndex = message['targetIndex'];
 
-                // TODO: update shots and ships
+                console.log('player ' + player.seat + ' targets ' + targetIndex);
 
-                // TODO: check for victory
+                let shots = player.seat == 1 ? this.state.player1Shots : this.state.player2Shots;
+                let targetPlayerIndex = player.seat == 1 ? 1 : 0;
+                let targetedPlacement = this.placements[targetPlayerIndex];
 
-                // ... else
+                if (targetedPlacement[targetIndex] > 0 && shots[targetIndex] == 0) {
+                    shots[targetIndex] = 1; // hit
+                    this.playerHealth[targetPlayerIndex]--;
+                } else if (targetedPlacement[targetIndex] == 0 && shots[targetIndex] == 0) {
+                    shots[targetIndex] = 2; // miss
+                }
 
-                this.state.playerTurn = this.state.playerTurn == 0 ? 1 : 0;
+                if (this.playerHealth[targetPlayerIndex] <= 0) {
+                    this.state.winningPlayer = player.seat;
+                    this.state.phase = "result";
+                } else {
+                    this.state.playerTurn = this.state.playerTurn == 1 ? 2 : 1;
+                }
             default:
                 console.log('unknown command');
         }
@@ -95,31 +110,29 @@ export class GameRoom extends Room<State> {
     }
 
     reset() {
+        this.playerHealth = new Array<number>();
+        this.playerHealth[0] = this.totalShipHealth;
+        this.playerHealth[1] = this.totalShipHealth;
+
         this.placements = new Array<Array<number>>();
         this.placements[0] = new Array<number>();
         this.placements[1] = new Array<number>();
-
-        this.playersPlaced = 0;
 
         let cellCount = this.gridSize * this.gridSize;
 
         let state = new State();
 
+        this.playersPlaced = 0;
         state.phase = 'waiting';
-        state.playerTurn = 0;
+        state.playerTurn = 1;
         state.winningPlayer = -1;
 
         for (var i=0; i<cellCount; i++) {
             this.placements[0][i] = 0;
             this.placements[1][i] = 0;
             
-            state.player1Shots[i] = '?';
-            state.player2Shots[i] = '?';
-        }
-
-        for (let shipType of shipTypes) {
-            state.player1Ships[shipType.id] = Ship.create(shipType.name, shipType.size);
-            state.player2Ships[shipType.id] = Ship.create(shipType.name, shipType.size);
+            state.player1Shots[i] = 0;
+            state.player2Shots[i] = 0;
         }
 
         this.setState(state);
--- src/state.ts
+++ src/state.ts
@@ -1,48 +1,24 @@
 import { Schema, type, MapSchema, ArraySchema } from "@colyseus/schema";
 
-export class Ship extends Schema {
-    @type('string')
-    name: string;
-
-    @type('number')
-    size: number;
-
-    @type('number')
-    health: number;
-
-    @type('string')
-    status: string;
-
-    static create(name: string, size: number): Ship {
-        var ship = new Ship();
-        ship.name = name;
-        ship.size = size;
-        ship.health = size;
-        ship.status = 'active';
-
-        return ship;
-    }
-}
-
 export class State extends Schema {
     @type('string')
     phase: string = "waiting";
 
-    @type('number')
+    @type('int16')
     playerTurn: number = 1;
 
     @type('number')
     winningPlayer: number = 0;
 
-    @type({ map: Ship })
-    player1Ships: MapSchema<Ship> = new MapSchema<Ship>();
+    @type('string')
+    player1: string = '';
 
-    @type({ map: Ship })
-    player2Ships: MapSchema<Ship> = new MapSchema<Ship>();
+    @type('string')
+    player2: string = '';
 
-    @type(['string'])
-    player1Shots: ArraySchema<string> = new ArraySchema<string>();
+    @type(['int16'])
+    player1Shots: ArraySchema<number> = new ArraySchema<number>();
 
-    @type(['string'])
-    player2Shots: ArraySchema<string> = new ArraySchema<string>();
+    @type(['int16'])
+    player2Shots: ArraySchema<number> = new ArraySchema<number>();
 }
