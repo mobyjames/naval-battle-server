f86dfc57a1d28b055119218bd12b71ac82560248
James Jacoby <james@mobyinc.com>
2019-10-15 19:32:20 -0700

    clean up main class

--- index.ts
+++ index.ts
@@ -1,22 +1,18 @@
-import path from 'path';
 import express from 'express';
-import serveIndex from 'serve-index';
 import { createServer } from 'http';
 import { Server } from 'colyseus';
 import { monitor } from '@colyseus/monitor';
-
-// Import demo room handlers
-import {  } from "./src/game-room";
 import { GameRoom } from "./src/game-room";
 
 const port = Number(process.env.PORT || 2567);
 const app = express();
 
-// Attach WebSocket Server on HTTP Server.
+// Create WebSocket Server
 const gameServer = new Server({
   server: createServer(app)
 });
 
+// Define a room type
 gameServer.register("game", GameRoom);
 
 // (optional) attach web monitoring panel
